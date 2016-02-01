5d598385b1b407fd30d85714f53800c7dcf295a8
olivierlauzon85 <olivierlauzon85@gmail.com>
2016-02-01 08:36:17 -0500

    création du projet
    la création du projet

---
+++ githubc/githubc/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace githubc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] table = new int[24];
+            table =Utilitaire.CreateRandomTable(table.Length, 0, 100);
+            Utilitaire.WriteTable(table);
+        }
+    }
+}
