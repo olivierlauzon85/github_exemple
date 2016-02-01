00ec0bff798a326c56471e463a9e321a7c3b2bdf
olivierlauzon85 <olivierlauzon85@gmail.com>
2016-02-01 08:37:24 -0500

    Revert "enlever utilitaire"
    This reverts commit 46c5273145c6d2a694a14b60c3a00bfe021f1e71.

---
+++ githubc/githubc/Utilitaire.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace githubc
+{
+    static class Utilitaire
+    {
+        public static void WriteTable(int[] Table)
+        {
+            for (int i = 0; i < Table.Length; ++i)
+            {
+                Console.WriteLine(Table[i]);
+            }
+        }
+        static Random Generator = new Random();
+
+        public static int GenerateRandomInt(int min, int max)
+        {
+            return Generator.Next(min, max + 1);
+        }
+
+        public static int[] CreateRandomTable(int length, int min, int max)
+        {
+            int[] randomTable = new int[length];
+
+            for (int cpt = 0; cpt < length; cpt++)
+            {
+                randomTable[cpt] = GenerateRandomInt(min, max);
+            }
+            return randomTable;
+        }
+    }
+}
